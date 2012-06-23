cd753dcf3d62a5620d849b5cdfaa6f3aa9a660cf
Jordan Sissel <jls@semicomplete.com>
2012-06-22 19:29:35 -0700

    Run templates on load

--- js/article.js
+++ js/article.js
@@ -3,4 +3,5 @@ $(document).ready(function() {
   Site.anchor("h1, h2, h3, h4, h5, h6");
   Site.table_of_contents("#content h1, #content h2, #content h3");
   Site.fix_intro("div.article");
+  Site.template("div.article");
 });
