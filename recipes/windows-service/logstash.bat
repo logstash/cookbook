29334978764d48ab0ca6b78a07375b037dbe9b98
Jordan Sissel <jls@semicomplete.com>
2013-12-16 10:04:55 -0800

    Update logstash.bat

--- recipes/windows-service/logstash.bat
+++ recipes/windows-service/logstash.bat
@@ -1,8 +1,7 @@
-
 C:
 cd \logstash
 
 set HOME=c:/logstash/sincedb
 
-"c:\Program Files\Java\jdk1.6.0_37\bin\java.exe" -Xmx32M -jar logstash-1.1.7-monolithic.jar agent --config logstash.conf --log logstash.log
+"c:\Program Files\Java\jdk1.6.0_37\bin\java.exe" -Xmx256m -jar logstash-1.1.7-monolithic.jar agent --config logstash.conf --log logstash.log
 
